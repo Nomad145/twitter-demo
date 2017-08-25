eab4d14cd487398b6b23e4ec6152cd16d3f136e0
Michael Phillips <michaeljoelphillips@gmail.com>
2017-08-24 19:56:44 -0500

    Update character sets for applicable columns.

--- config/migrations/20170822.sql
+++ config/migrations/20170822.sql
@@ -1,28 +1,24 @@
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
-    handle VARCHAR(15) NOT NULL,
+    handle VARCHAR(15) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+    email VARCHAR(255) NOT NULL,
     CONSTRAINT user_pk PRIMARY KEY (id),
     UNIQUE (handle),
     UNIQUE (email)
-)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_bin;
+);
 
 CREATE TABLE tweet
 (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT,
-    message VARCHAR(140) NOT NULL,
+    message VARCHAR(140) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
     date_posted DATETIME NOT NULL,
     INDEX user_id (user_id),
     FOREIGN KEY (user_id)
         REFERENCES user(id)
         ON DELETE RESTRICT,
     CONSTRAINT tweet_pk PRIMARY KEY (id)
-)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_bin;
+);
